10fcfd3206f7967be17976833d10332b9e115587
Carlos <carlos_buske@hotmail.com>
2025-03-16 15:48:59 -0300

    adicionando classes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,18 +1,12 @@
 import express from "express";
-import {
-  userGet,
-  userGetOne,
-  userDelete,
-  userUpdate,
-  UserControllers,
-} from "../controllers/controller";
+import { UserControllers } from "../controllers/controller";
 
 const router = express.Router();
 export default router;
 const userController = new UserControllers();
 
 router.post("/createuser", userController.userPost);
-router.get("/createuser", userGet);
-router.get("/createuser/:id_user", userGetOne);
-router.put("/createuser/:id_user", userUpdate);
-router.delete("/createuser/:id_user", userDelete);
+router.get("/createuser", userController.userGet);
+router.get("/createuser/:id_user", userController.userGetOne);
+router.put("/createuser/:id_user", userController.userPut);
+router.delete("/createuser/:id_user", userController.userDelete);
--- src/service/service.ts
+++ src/service/service.ts
@@ -1,7 +1,7 @@
 import { AppDataSource } from "../database/data-source";
 import { User } from "../entity/entity";
 
-export async function crateUser(
+/*export async function crateUser(
   nome: string,
   email: string,
   password: string,
@@ -15,4 +15,25 @@ export async function crateUser(
 
   const userRepository = AppDataSource.getRepository(User);
   await userRepository.save(newUser);
+}*/
+
+export class UserService {
+  private userRepository;
+  constructor() {
+    this.userRepository = AppDataSource.getRepository(User);
+  }
+  async createUser(
+    nome: string,
+    email: string,
+    password: string,
+    user: string
+  ) {
+    const newUser = new User();
+    newUser.name = nome;
+    newUser.email = email;
+    newUser.password = password;
+    newUser.user = user;
+
+    await this.userRepository.save(newUser);
+  }
 }
