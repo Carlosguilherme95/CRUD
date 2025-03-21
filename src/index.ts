18c6ef937bdfe61b724f64bcc289116bb49ae128
Carlos <carlos_buske@hotmail.com>
2025-03-21 20:28:40 -0300

    adicionando swagger

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,33 @@
 import { AppDataSource } from "../src/database/data-source";
 import express, { Request, Response } from "express";
 import routes from "../src/routes/routes";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use("/api", routes);
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Minha API",
+      version: "1.0.0",
+      description: "API de exemplo com Swagger e TypeScript",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./src/routes/routes.ts"], // Caminho para o arquivo de rotas
+};
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 async function connect() {
   try {
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -6,12 +6,154 @@ const router = express.Router();
 
 const userController = new UserControllers();
 
+/**
+ * @swagger
+ * /createuser:
+ *   post:
+ *     description: Cria um novo usuário
+ *     parameters:
+ *       - in: body
+ *         name: user
+ *         description: O usuário a ser criado
+ *         required: true
+ *         schema:
+ *           type: object
+ *           required:
+ *             - name
+ *             - email
+ *             - password
+ *             - user
+ *           properties:
+ *             name:
+ *               type: string
+ *             email:
+ *               type: string
+ *             password:
+ *               type: string
+ *             user:
+ *               type: string
+ *     responses:
+ *       200:
+ *         description: Usuário criado com sucesso
+ *       400:
+ *         description: Dados inválidos
+ */
 router.post("/createuser", userController.userPost);
+
+/**
+ * @swagger
+ * /createuser:
+ *   get:
+ *     description: Retorna todos os usuários
+ *     responses:
+ *       200:
+ *         description: Lista de usuários
+ */
 router.get("/createuser", userController.getAllUser);
+
+/**
+ * @swagger
+ * /createuser/{id_user}:
+ *   get:
+ *     description: Retorna um único usuário pelo ID
+ *     parameters:
+ *       - in: path
+ *         name: id_user
+ *         required: true
+ *         description: ID do usuário a ser retornado
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Usuário encontrado
+ *       404:
+ *         description: Usuário não encontrado
+ */
 router.get("/createuser/:id_user", userController.userGetOne);
+
+/**
+ * @swagger
+ * /createuser/{id_user}:
+ *   put:
+ *     description: Modifica os dados de um usuário
+ *     parameters:
+ *       - in: path
+ *         name: id_user
+ *         required: true
+ *         description: ID do usuário a ser modificado
+ *         schema:
+ *           type: integer
+ *       - in: body
+ *         name: user
+ *         description: Dados atualizados do usuário
+ *         required: true
+ *         schema:
+ *           type: object
+ *           properties:
+ *             name:
+ *               type: string
+ *             email:
+ *               type: string
+ *             password:
+ *               type: string
+ *             user:
+ *               type: string
+ *     responses:
+ *       200:
+ *         description: Usuário atualizado com sucesso
+ *       400:
+ *         description: Dados inválidos
+ *       404:
+ *         description: Usuário não encontrado
+ */
 router.put("/createuser/:id_user", userController.userPut);
+
+/**
+ * @swagger
+ * /createuser/{id_user}:
+ *   delete:
+ *     description: Deleta um usuário pelo ID
+ *     parameters:
+ *       - in: path
+ *         name: id_user
+ *         required: true
+ *         description: ID do usuário a ser deletado
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Usuário deletado com sucesso
+ *       404:
+ *         description: Usuário não encontrado
+ */
 router.delete("/createuser/:id_user", userController.userDelete);
 
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     description: Realiza o login de um usuário e retorna um token JWT
+ *     parameters:
+ *       - in: body
+ *         name: login
+ *         description: Credenciais de login do usuário
+ *         required: true
+ *         schema:
+ *           type: object
+ *           required:
+ *             - user
+ *             - password
+ *           properties:
+ *             user:
+ *               type: string
+ *             password:
+ *               type: string
+ *     responses:
+ *       200:
+ *         description: Login bem-sucedido, retorna o token JWT
+ *       400:
+ *         description: Credenciais inválidas
+ */
 router.post("/login", userController.userLogin);
 
 export default router;
